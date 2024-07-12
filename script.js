9a1efac605fb4c4d2fe9b9345d3880ab36f06a42
SriMoorthy <srimoorthy27@gmail.com>
2024-07-12 15:43:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 //your JS code here. If required.
-function clickablediv(){
-    document.getElementById('square2').style.background= '#6F4E37'
-	document.getElementById('square3').style.background= '#6F4E37'
-}
+let sq1 = document.getElementById('square1')
+let sq2 = document.getElementById('square2')
+let sq3 = document.getElementById('square3')
+sq1.addEventListener('mouseenter',() => {
+	document.getElementById('square2').style.background='#6F4E37'
+	document.getElementById('square3').style.background='#6F4E37'
+})
 
-function clickablediv1() {
-	document.getElementById('square1').style.background= '#6F4E37'
-	document.getElementById('square3').style.background= '#6F4E37'
-}
+sq1.addEventListener('mouseleave',() => {
+	document.getElementById('square2').style.background='#E6E6FA'
+	document.getElementById('square3').style.background='#E6E6FA'
+})
 
-function clickablediv2() {
-	document.getElementById('square1').style.background= '#6F4E37'
-	document.getElementById('square2').style.background= '#6F4E37'
-}
+sq2.addEventListener('mouseenter',() => {
+	document.getElementById('square1').style.background='#6F4E37'
+	document.getElementById('square3').style.background='#6F4E37'
+})
+
+sq2.addEventListener('mouseleave',() => {
+	document.getElementById('square1').style.background='#E6E6FA'
+	document.getElementById('square3').style.background='#E6E6FA'
+})
+
+sq3.addEventListener('mouseenter',() => {
+	document.getElementById('square1').style.background='#6F4E37'
+	document.getElementById('square2').style.background='#6F4E37'
+})
+
+sq3.addEventListener('mouseleave',() => {
+	document.getElementById('square1').style.background='#E6E6FA'
+	document.getElementById('square2').style.background='#E6E6FA'
+})
+
