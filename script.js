a957c318b8956559e288d6f5c910e3d32d08d753
SriMoorthy <srimoorthy27@gmail.com>
2024-07-12 15:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function clickablediv(){
+    document.getElementById('square2').style.background= '#6F4E37'
+	document.getElementById('square3').style.background= '#6F4E37'
+}
+
+function clickablediv1() {
+	document.getElementById('square1').style.background= '#6F4E37'
+	document.getElementById('square3').style.background= '#6F4E37'
+}
+
+function clickablediv2() {
+	document.getElementById('square1').style.background= '#6F4E37'
+	document.getElementById('square2').style.background= '#6F4E37'
+}
