6566ec17c904146dad6f811577758467342f842a
SriMoorthy <srimoorthy27@gmail.com>
2024-07-12 15:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ let sq1 = document.getElementById('square1')
 let sq2 = document.getElementById('square2')
 let sq3 = document.getElementById('square3')
 sq1.addEventListener('mouseenter',() => {
-	document.getElementById('square2').style.background='#6F4E37'
-	document.getElementById('square3').style.background='#6F4E37'
+	document.getElementById('square2').style.background='rgb(111, 78, 55)'
+	document.getElementById('square3').style.background='rgb(111, 78, 55)'
 })
 
 sq1.addEventListener('mouseleave',() => {
-	document.getElementById('square2').style.background='#E6E6FA'
-	document.getElementById('square3').style.background='#E6E6FA'
+	document.getElementById('square2').style.background='rgb(230, 230, 250)'
+	document.getElementById('square3').style.background='rgb(230, 230, 250)'
 })
 
 sq2.addEventListener('mouseenter',() => {
-	document.getElementById('square1').style.background='#6F4E37'
-	document.getElementById('square3').style.background='#6F4E37'
+	document.getElementById('square1').style.background='rgb(111, 78, 55)'
+	document.getElementById('square3').style.background='rgb(111, 78, 55)'
 })
 
 sq2.addEventListener('mouseleave',() => {
-	document.getElementById('square1').style.background='#E6E6FA'
-	document.getElementById('square3').style.background='#E6E6FA'
+	document.getElementById('square1').style.background='rgb(230, 230, 250)' 
+	document.getElementById('square3').style.background='rgb(230, 230, 250)' 
 })
 
 sq3.addEventListener('mouseenter',() => {
-	document.getElementById('square1').style.background='#6F4E37'
-	document.getElementById('square2').style.background='#6F4E37'
+	document.getElementById('square1').style.background='rgb(111, 78, 55)'
+	document.getElementById('square2').style.background='rgb(111, 78, 55)'
 })
 
 sq3.addEventListener('mouseleave',() => {
-	document.getElementById('square1').style.background='#E6E6FA'
-	document.getElementById('square2').style.background='#E6E6FA'
+	document.getElementById('square1').style.background='rgb(230, 230, 250)'
+	document.getElementById('square2').style.background='rgb(230, 230, 250)'
 })
 
