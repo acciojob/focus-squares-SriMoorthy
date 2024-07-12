750211b8dbedb000ccf14b16a8b3a8750358ccbe
SriMoorthy <srimoorthy27@gmail.com>
2024-07-12 16:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,23 +19,4 @@
             });
         }); 
 
-describe('Color Change Squares', () => {
-    const baseUrl = 'http://localhost:3000'; // Update the URL if necessary
-
-    it('changes colors on hover', () => {
-        cy.visit(baseUrl);
-        
-        // Trigger mouseover on square1 and verify colors
-        cy.get('#square1').trigger('mouseover');
-        cy.get('#square1').should('have.css', 'background-color', 'rgb(230, 230, 250)'); // Lavender
-        cy.get('#square2').should('have.css', 'background-color', 'rgb(111, 78, 55)'); // Coffee
-        cy.get('#square3').should('have.css', 'background-color', 'rgb(111, 78, 55)'); // Coffee
-        
-        // Trigger mouseout on square1 and verify colors
-        cy.get('#square1').trigger('mouseout');
-        cy.get('#square1').should('have.css', 'background-color', 'rgb(230, 230, 250)'); // Lavender
-        cy.get('#square2').should('have.css', 'background-color', 'rgb(230, 230, 250)'); // Lavender
-        cy.get('#square3').should('have.css', 'background-color', 'rgb(230, 230, 250)'); // Lavender
-    });
-});
 
