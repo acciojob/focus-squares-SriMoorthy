6b9dc77be2ca4076f5117185a768431e01bebf14
SriMoorthy <srimoorthy27@gmail.com>
2024-07-12 16:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,19 +4,21 @@
 
             squares.forEach((square, index) => {
                 square.addEventListener('mouseenter', () => {
+                    console.log(`Mouse entered square ${index + 1}`);
                     squares.forEach((sq, i) => {
                         if (i !== index) {
                             sq.style.backgroundColor = '#6F4E37'; // Coffee
+                            console.log(`Square ${i + 1} color changed to Coffee`);
                         }
                     });
                 });
 
                 square.addEventListener('mouseleave', () => {
+                    console.log(`Mouse left square ${index + 1}`);
                     squares.forEach(sq => {
                         sq.style.backgroundColor = '#E6E6FA'; // Lavender
+                        console.log(`Square color reset to Lavender`);
                     });
                 });
             });
-        }); 
-
-
+        });
